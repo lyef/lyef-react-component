4d5910058fa2eac56a4734f74d2c04dc15ff5d8b
Willian Justen <willianjustenqui@gmail.com>
2016-07-10 02:44:16 -0300

    Add storybook

---
+++ stories/Foo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Foo from '../src/Foo'; // This is our component
+import { storiesOf, action } from '@kadira/storybook';
+
+storiesOf('Foo', module)
+  .add('with 1 item', () => (
+    <Foo itens={[1]} />
+  ))
+  .add('with 3 itens', () => (
+    <Foo itens={[1,2,3]} />
+  ))
+  .add('with text itens', () => (
+    <Foo itens={['react', 'awesome']} />
+  ))
